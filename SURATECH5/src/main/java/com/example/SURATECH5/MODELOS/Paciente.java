7cfa8a4939e189c3c70185fc81e8f2d27b2c09b3
Samuel <mazosamuel023@gmail.com>
2024-09-16 12:14:27 -0500

    se crean modelos de datos

---
+++ SURATECH5/src/main/java/com/example/SURATECH5/MODELOS/Paciente.java
@@ -0,0 +1,30 @@
+package com.example.SURATECH5.MODELOS;
+
+public class Paciente {
+    String nombres;
+
+    String cedula;
+
+    Integer edad;
+
+    Integer ips;
+
+    String correo;
+
+    String RH;
+
+    Double peso;
+
+    Double estatura;
+
+    String telefono;
+
+    String estadoCivil;
+
+    String direccion;
+
+    String ciudad;
+
+
+
+}
---
+++ SURATECH5/src/main/java/com/example/SURATECH5/MODELOS/SignoVital.java
@@ -0,0 +1,14 @@
+package com.example.SURATECH5.MODELOS;
+
+import java.time.LocalDate;
+
+public class SignoVital {
+
+    String nombre;
+
+    Integer valor;
+
+    LocalDate fechaTomo;
+
+    String descripcion;
+}
